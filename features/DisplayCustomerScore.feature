b1484f05d4dd4b5bfe86b1e8a9503900096e7249
JeffullQA <jeff.sapotille@algoan.com>
2021-02-09 09:25:16 -0400

    add scenario

--- features/DisplayCustomerScore.feature
+++ features/DisplayCustomerScore.feature
@@ -1,6 +1,6 @@
 Feature: Display customer score
   
   Scenario: Display score
-    Given I want to display the score of a customer
-    When I click on the client's file
+    Given I want to display the score of a client
+    When I click on the client file
     Then the score is displayed
--- features/step-definitions/stepdef.js
+++ features/step-definitions/stepdef.js
@@ -1,2 +1,21 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+function DisplayScore(file) {
+    
+}
+
+Given('I want to display the score of a client', function () {
+    // Write code here that turns the phrase above into concrete actions
+    this.file = 'score'
+  });
+
+When('I click on the client file', function () {
+    // Write code here that turns the phrase above into concrete actions
+    this.clickFile = DisplayScore(this.file);
+  });
+
+Then('the score is displayed', function (expectedAnswer) {
+    // Write code here that turns the phrase above into concrete actions
+    assert.equal(this.clickFile, expectedAnswer);
+  });
